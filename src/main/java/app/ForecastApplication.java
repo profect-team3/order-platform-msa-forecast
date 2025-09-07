f290790e02f54440650e74c2be8fd386a067ad3f
ColdTbrew <shchoi8687@hotmail.com>
2025-09-08 00:21:27 +0900

    init ForecastApplication

--- src/main/java/app/ForecastApplication.java
+++ src/main/java/app/ForecastApplication.java
@@ -1,4 +1,4 @@
-package com.example.orderplatformmsaforecast;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/app/OrderPlatformMsaForecastApplicationTests.java
+++ src/test/java/app/OrderPlatformMsaForecastApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.orderplatformmsaforecast;
+package app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
