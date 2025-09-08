1f1d743f92099d607d2c5cfa476b1e29749becfe
ColdTbrew <shchoi8687@hotmail.com>
2025-09-08 23:35:16 +0900

    fix(forecast): 샘플 테스트 코드 작성

--- src/test/java/app/ForecastApplicationTests.java
+++ src/test/java/app/ForecastApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderPlatformMsaForecastApplicationTests {
+class ForecastApplicationTests {
 
 	@Test
 	void contextLoads() {
