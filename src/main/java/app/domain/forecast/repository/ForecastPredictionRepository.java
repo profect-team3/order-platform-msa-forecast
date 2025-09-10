9d44e6db3bc138fee9d98fd4e96a894c85f8dd7d
ColdTbrew <shchoi8687@hotmail.com>
2025-09-11 00:22:45 +0900

    feat(forecast): Kafka Streams를 이용한 주문 통계 실시간 집계 기능 구현

--- src/main/java/app/domain/forecast/repository/ForecastPredictionRepository.java
+++
@@ -1,7 +0,0 @@
-package app.domain.forecast.repository;
-
-import app.domain.forecast.document.ForecastPrediction;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ForecastPredictionRepository extends MongoRepository<ForecastPrediction, String> {
-}
