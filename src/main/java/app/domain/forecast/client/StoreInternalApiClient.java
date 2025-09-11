4c23c922e84636edc587e971aec55db6bade3c5d
seongjoow <seongjoow@gmail.com>
2025-09-11 18:36:30 +0900

    Feat(forecast) : Store 서비스 클라이언트와 DTO 수정

--- src/main/java/app/domain/forecast/client/StoreInternalApiClient.java
+++ src/main/java/app/domain/forecast/client/StoreInternalApiClient.java
@@ -1,15 +1,16 @@
 package app.domain.forecast.client;
 
 import app.commonUtil.apiPayload.ApiResponse;
-import app.domain.forecast.model.dto.response.StoreServiceStoreInfo;
+import app.domain.forecast.model.dto.response.StoreCollection;
 
+import app.config.FeignClientAuthConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "store")
+@FeignClient(name = "store-internal-api", url = "http://localhost:8082", configuration = FeignClientAuthConfig.class)
 public interface StoreInternalApiClient {
 
-    @GetMapping("/internal/store/mongo/{storeId}")
-    ApiResponse<StoreServiceStoreInfo> getStoreByKey(@PathVariable("storeId") String storeKey);
+    @GetMapping("/store/mongo/{storeId}")
+    ApiResponse<StoreCollection> getStoreByKey(@PathVariable("storeId") String storeKey);
 }
